05461f87d01034a8ed60ce2763709f39817f1072
polesskiy-dev <e34dream@gmail.com>
2016-06-29 01:41:32 +0300

    Start refactoring spec tests - add async waterfall

--- spec/user-crud.spec.js
+++ spec/user-crud.spec.js
@@ -1,5 +1,10 @@
+/**
+ * @see <a href="https://www.npmjs.com/package/jasmine-node"></a>
+ * @see <a href="https://www.npmjs.com/package/async"></a>
+ */
 const request = require("request");
 const util = require('util');
+const async = require('async');
 const nconf = require('nconf');
 
 // get test user obj
@@ -20,61 +25,99 @@ const TEST_USER_URL = `${USERS_API_URL}/${testUser.login}`;
  * Test user CRUD
  */
 describe("CRUD", function () {
-    let authToken = null;
+    let authToken = "";
 
-    /** create new test user*/
-    beforeEach(function (done) {
-        request.post({
-            headers: {'content-type': 'application/json'},
-            url: REGISTRATION_URL,
-            body: JSON.stringify(testUser)
-        }, function (error, response, body) {
-            console.log("Creating new user response: %s", body);
-            expect(response.statusCode).toBe(200);
+    // /** create new test user*/
+    // xbeforeEach(function (done) {
+    //     async.waterfall([
+    //             function (callback) {
+    //                 console.log("sending user registration request");
+    //                 request.post({
+    //                     headers: {'content-type': 'application/json'},
+    //                     url: REGISTRATION_URL,
+    //                     body: JSON.stringify(testUser)
+    //                 }, callback(error, response, body))
+    //             },
+    //             function (error, response, body, next) {
+    //                 console.log("Creating new user response: %j", body);
+    //                 expect(response.statusCode).toBe(200);
+    //                 callback(null, body);
+    //             }
+    //         ],
+    //         function (err, result) {
+    //             console.log("finally, res: %j", body);
+    //             done();
+    //         }
+    //     )
+    //     ;
+    //
+    //     // request.post({
+    //     //     headers: {'content-type': 'application/json'},
+    //     //     url: REGISTRATION_URL,
+    //     //     body: JSON.stringify(testUser)
+    //     // }, function (error, response, body) {
+    //     //     console.log("Creating new user response: %s", body);
+    //     //     expect(response.statusCode).toBe(200);
+    //     //
+    //     //     //log in
+    //     //     request.post({
+    //     //             headers: {'content-type': 'application/json'},
+    //     //             url: LOGIN_URL,
+    //     //             body: JSON.stringify(testUser)
+    //     //         },
+    //     //         function (error, response, body) {
+    //     //             // console.log("token:\r\n%s", JSON.parse(body).token);
+    //     //             authToken = JSON.parse(body).token;
+    //     //             expect(response.statusCode).toBe(200);
+    //     //             done();
+    //     //         });
+    //     // });
+    // });
+    //
+    // /** delete recently created test user */
+    // xafterEach(function (done) {
+    //     request({
+    //             headers: {'Authorization': authToken},
+    //             method: 'DELETE',
+    //             url: TEST_USER_URL
+    //         },
+    //         function (error, response, body) {
+    //             console.log("Response for single user: \r\n%s", body);
+    //             expect(response.statusCode).toBe(200);
+    //             done();
+    //         }
+    //     );
+    // });
 
-            //log in
-            request.post({
-                    headers: {'content-type': 'application/json'},
-                    url: LOGIN_URL,
+    it("GET single user, returns status code 200", function (done) {
+        // request.get({
+        //     headers: {'Authorization': authToken},
+        //     url: TEST_USER_URL,
+        //     body: JSON.stringify(testUser)
+        // }, function (error, response, body) {
+        //     console.log("Response for single user: \r\n%j", body);
+        //     expect(response.statusCode).toBe(200);
+        //     done();
+        // });
+
+        async.waterfall([
+            function (callback) {
+                console.log("Sending request");
+                request.get({
+                    headers: {'Authorization': authToken},
+                    url: TEST_USER_URL,
                     body: JSON.stringify(testUser)
-                },
-                function (error, response, body) {
-                    // console.log("token:\r\n%s", JSON.parse(body).token);
-                    authToken = JSON.parse(body).token;
-                    expect(response.statusCode).toBe(200);
-                    done();
-                });
-        });
-    });
-
-    /** delete recently created test user */
-    afterEach(function (done) {
-        request({
-                headers: {'Authorization': authToken},
-                method: 'DELETE',
-                url: TEST_USER_URL
+                }, callback);
             },
-            function (error, response, body) {
-                console.log("Response for single user: \r\n%s", body);
+            function (response, body) {
+                console.log("Response for single user: \r\n%j", body);
                 expect(response.statusCode).toBe(200);
                 done();
             }
-        );
+        ]);
     });
 
-    it("GET single user, returns status code 200", function (done) {
-        request.get({
-            headers: {'Authorization': authToken},
-            url: TEST_USER_URL,
-            body: JSON.stringify(testUser)
-        }, function (error, response, body) {
-            console.log("Response for single user: \r\n%j", body);
-            expect(response.statusCode).toBe(200);
-            done();
-        });
-    });
-
-    it("GET all users from not-admin user", function (done) {
+    xit("GET all users from not-admin user", function (done) {
         request.get({
             headers: {'Authorization': authToken},
             url: USERS_API_URL
@@ -83,4 +126,5 @@ describe("CRUD", function () {
             done();
         });
     });
-});
+})
+;
