ca606d015518700f8823c8d959e87377f85c5c8a
polesskiy <polesskiy.dev@gmail.com>
2016-07-10 16:47:15 +0300

    edit viewport

--- public/js/admin/admin-page.js
+++ public/js/admin/admin-page.js
@@ -34,7 +34,7 @@ adminApp.controller("UsersCtrl", function ($scope, $http) {
     $http.get("/api/users")
         .then(function (response) {
             $scope.users = response.data;
-        });
+        })
 });
 
 /**
